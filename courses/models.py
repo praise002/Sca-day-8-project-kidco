addce530c292d62a0ff6f6352540215cfe02e796
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-10-12 08:27:40 -0500

    Created content models using abstract models

--- courses/models.py
+++ courses/models.py
@@ -41,6 +41,41 @@ class Module(models.Model):
     
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, 
+                                    on_delete=models.CASCADE,
+                                    limit_choices_to={'model__in':(  #model__in: to filter query
+                                        'text',
+                                        'video',
+                                        'image',
+                                        'file'
+                                    )})  #limit_choices is used for d generic relationship
     object_id = models.PositiveIntegerField()
-    item = GenericForeignKey('content_type', 'object_id')
+    item = GenericForeignKey('content_type', 'object_id')  #it combines the two previous fields
+    #The item allows you to retrieve or set the related object directly and its functionality is built
+    #on top of the other two fields
+    
+    
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        
+    def __str__(self):
+        return self.title
+    
+
+class Text(ItemBase):  #to store text content
+    content = models.TextField()
+
+class File(ItemBase):  #to store files such as pdfs
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):  #to store image files
+    image = models.FileField(upload_to='images')
+
+class Video(ItemBase):  # to store videos
+    url = models.URLField()  #to embed the video
